b1e2b1c8487691189db747e012ada32dfc90a27b
stoffee <chris@roadkill.org>
2020-04-22 21:39:44 -0700

    updated aws output

--- aws/output.tf
+++ aws/output.tf
@@ -1,3 +1,12 @@
 output "instance_ip_addr" {
   value = aws_instance.rosettaathome.public_ip
 }
+
+output "instance_login_information" {
+  value = <<INSTANCEIP
+
+Your Rosetta@home AWS instance has been created
+  $ ssh -i aws-ssh-key.pem ubuntu@${aws_instance.rosettaathome.public_ip}
+
+INSTANCEIP
+}
