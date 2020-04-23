b0d96a2ded32530f77a0a787e7bbb9480ca11fd1
stoffee <chris@roadkill.org>
2020-04-22 22:19:35 -0700

    updated azure provider

--- azure/output.tf
+++ azure/output.tf
@@ -1,8 +1,7 @@
 output "azure_instance_info" {
   value = <<INSTANCEIP
-
 Your Rosetta@home Azure instance has been created
   $ ssh  ${var.ssh_admin_username}@${data.azurerm_public_ip.rosetta-public-ip.ip_address}
   password: ${var.ssh_admin_password}
-
 INSTANCEIP
+}
