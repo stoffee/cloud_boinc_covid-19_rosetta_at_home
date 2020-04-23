dcf2b61131691f5126d64a19d0819a1522a2edb6
stoffee <chris@roadkill.org>
2020-04-22 23:58:09 -0700

    updated output

--- alicloud/outputs.tf
+++ alicloud/outputs.tf
@@ -1,6 +1,6 @@
 output "alicloud_ecs_instance_info" {
   value = <<INSTANCEIP
 Your Rosetta@home Alicloud ECS instance has been created
-  $ ssh -i ${var.key_pair}  root@${alicloud_instance.website.public_ip}
+  $ ssh -i ${var.key_name}  root@${alicloud_instance.website.public_ip}
 INSTANCEIP
 }
