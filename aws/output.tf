f091a813e64a7561bb54e09d4711e39004986dc7
stoffee <chris@roadkill.org>
2020-04-22 21:47:12 -0700

    updated aws output

--- aws/output.tf
+++ aws/output.tf
@@ -1,8 +1,4 @@
-output "instance_ip_addr" {
-  value = aws_instance.rosettaathome.public_ip
-}
-
-output "instance_login_information" {
+output "aws_instance_login_information" {
   value = <<INSTANCEIP
 
 Your Rosetta@home AWS instance has been created
