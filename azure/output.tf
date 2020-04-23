f3f84d7db5645ee355b161056fe12fadd8012475
stoffee <chris@roadkill.org>
2020-04-22 22:10:03 -0700

    updated azure provider

--- azure/output.tf
+++ azure/output.tf
@@ -1,3 +1,8 @@
-output "appGatewayIp" {
-  value = data.azurerm_public_ip.rosetta-public-ip.ip_address
-}
+output "azure_instance_info" {
+  value = <<INSTANCEIP
+
+Your Rosetta@home Azure instance has been created
+  $ ssh  ${var.ssh_admin_username}@${data.azurerm_public_ip.rosetta-public-ip.ip_address}
+  password: ${var.ssh_admin_password}
+
+INSTANCEIP
